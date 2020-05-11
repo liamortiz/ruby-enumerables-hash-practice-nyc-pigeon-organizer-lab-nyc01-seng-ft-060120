1b071184117b11b2313295b4f37fdb38ca4acd3c
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-11 22:09:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,18 +2,18 @@ def nyc_pigeon_organizer(data)
   sorted_hash = {} # Create an empty hash to return later on
   
   data.each do |parent_key, parent_value|
-    parent_value.each do |child_key, child_value| #child_key = colors, genders, places , child_value = name arrays
+    parent_value.each do |child_key, child_value|
+      child_value.each do |name| # Loop through all the names and values and assign
+      
+        if child_value.include?(name)
+          if !sorted_hash[name] # Creates the name key object if it doesn't have one
+            sorted_hash[name] = {parent_key => [child_key.to_s]}
   
-      child_value.each do |pigeon_name| # For each name assign its value
-        if child_value.include?(pigeon_name)
-          if !sorted_hash[pigeon_name] # Creates the object if it doesn't already have one
-            sorted_hash[pigeon_name] = {parent_key => [child_key.to_s]}
-  
-          elsif !sorted_hash[pigeon_name].key?(parent_key) # If the key is not set, add it with the first value
-            sorted_hash[pigeon_name][parent_key] = [child_key.to_s]
+          elsif !sorted_hash[name].key?(parent_key) # If the key is not set, add it with the first value
+            sorted_hash[name][parent_key] = [child_key.to_s]
   
           else # Else if the key is found append to the current values of that key
-            sorted_hash[pigeon_name][parent_key] << child_key.to_s
+            sorted_hash[name][parent_key] << child_key.to_s
           end
         end
       end
