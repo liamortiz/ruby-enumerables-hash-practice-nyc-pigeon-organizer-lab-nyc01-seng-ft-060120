d37243def1e38e02aa4272bf243b0c2394e385a7
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-11 22:15:47 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  sorted_hash = {} # Create an empty hash to return later
+  
+  data.each do |parent_key, parent_value|
+    parent_value.each do |child_key, child_value|
+      child_value.each do |name|
+        
+        # This is our main block where the data gets sorted
+        if child_value.include?(name)
+          if not sorted_hash[name]
+            # The object with the name key is not found, make one.
+            sorted_hash[name] = {parent_key => [child_key.to_s]}
+            
+          elsif not sorted_hash[name].key?(parent_key)
+            # The name key is not in the object/hash, make one.
+            sorted_hash[name][parent_key] = [child_key.to_s]
+            
+          else
+            # Object/hash already made and contains current values, update them.
+            sorted_hash[name][parent_key] << child_key.to_s
+          end
+        end
+      end
+    end
+  end
+  return sorted_hash
 end
