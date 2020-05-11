ffbfd0bac65c665e110d152233cfdad9264b877f
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-11 22:09:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,7 +9,8 @@ def nyc_pigeon_organizer(data)
           if !sorted_hash[name] # Creates the name key object if it doesn't have one
             sorted_hash[name] = {parent_key => [child_key.to_s]}
   
-          elsif !sorted_hash[name].key?(parent_key) # If the key is not set, add it with the first value
+          elsif !sorted_hash[name].key?(parent_key)
+              # If the key is not set, add it with the first value
             sorted_hash[name][parent_key] = [child_key.to_s]
   
           else # Else if the key is found append to the current values of that key
